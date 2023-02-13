cbaa48d02f55d2852ce5618250b4c9a739c2f4b3
Camila Machado <sra.mac18@gmail.com>
2023-02-13 20:20:21 -0300

    Project version 1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function calculateTip(event){
+    event.preventDefault();
+    let bill = document.getElementById('bill').value;
+    let serviceQual = document.getElementById('serviceQual').value;
+    let people = document.getElementById('people').value;
+
+    if(bill=='' || serviceQual<=0){
+        alert('Por favor, preencha os campos corretamente!');
+    }
+    
+    let total = (bill * serviceQual)/people;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display="block";
+
+    if(people == '' || people <=1){
+        document.getElementById('each').style.display="none";
+        return false;
+    }else{
+        document.getElementById('each').style.display="block";
+    }
+}
+
+document.getElementById('totalTip').style.display="none";
+document.getElementById('each').style.display="none";
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
